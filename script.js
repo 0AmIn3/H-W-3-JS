2db8c5a6e10caab314633bdc6543979c22a068a3
0AmIn3 <aminfedya2888@gmail.com>
2022-06-12 01:16:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+////////////////////////////////////////////// Задание №1
+
+
+// let a = prompt("What is your name?")
+
+// if(a[0].toLowerCase() === "b" && (a.length & 1) == 0 === true ){
+//     console.log("Welcome")
+// }
+// else{
+//     console.log("Get out")
+// }
+
+
+
+
+
+
+////////////////////////////////////////////// Задание №2
+
+
+// let str = prompt('What is your name?')
+
+//     console.log(str.trim())
+ 
+
+////////////////////////////////////////////// Задание №3
+
+// let c = prompt("Numder")
+
+// if((c & 1) == 0 === true){
+//     console.log(alert("even"))
+// }
+// else{
+//     console.log(alert("odd"))
+// }
+    
+
+
+
+
+
+
